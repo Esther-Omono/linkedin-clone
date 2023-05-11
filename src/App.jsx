95e745fe1fc012ed89f63702c464296bf7718f91
Esther-Omono <enitanatakere@gmail.com>
2023-05-11 21:08:09 +0100

    Added header

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+    return (
+        <>
+            {/* Header */}
+            <Header />
+            {/* App Body */}
+                {/* Sidebar */}
+                {/* Feed */}
+                {/* Widgets */}
+        </>
+    )
+}
+
+export default App
